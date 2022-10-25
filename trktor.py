9e26c5dbc72eab5c7deb0dc7d6b4f0c9f1a8ef29
ma <ma@sep.de>
2022-10-25 10:06:34 +0200

    more obstacles

--- trktor.py
+++ trktor.py
@@ -5,6 +5,9 @@ Simons jump game
 import sys
 import math
 from dataclasses import dataclass
+from glob import glob
+import json
+import random
 import pygame
 
 
@@ -27,11 +30,6 @@ class surface:
 
 
 @dataclass
-class obstacles:
-    obstacle: list
-
-
-@dataclass
 class game:
     screen_w: int = 800
     screen_h: int = 800
@@ -43,6 +41,9 @@ class game:
     caption: str = "simons traktor spiel"
     tiles: int = 0
     jumps: bool = False
+    media = "assets/"
+    media_obstacles = f"{media}/obstacles/"
+    coins: int = 5
 
 
 @dataclass
@@ -56,12 +57,26 @@ class vehicle:
     x_start: int = 200
 
 
+def load_obstacles(path):
+    obstacles = []
+    for odir in glob(f"{path}/*/"):
+        with open(f"{odir}/info.json", "r") as j:
+            info = json.loads(j.read())
+        this = obstacle(
+            asset=f"{odir}/img.png", width=info["width"], height=info["height"]
+        )
+        obstacles.append(this)
+
+    return obstacles
+
+
 def main():
     pygame.init()
 
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((game.screen_w, game.screen_h))
     pygame.display.set_caption(game.caption)
+    gameobj = game()
 
     vh_standing = vehicle(asset="assets/trktor_standing.png", height=99, width=64)
     vh_jumping = vehicle(
@@ -83,17 +98,22 @@ def main():
         ),
         (vh_jumping.height, vh_jumping.width),
     )
-    obstacle_ = obstacle(asset="assets/rock.png", width=77, height=64)
+    obstacles = load_obstacles(game.media_obstacles)
+    obstacle_ = obstacles[random.randrange(0, len(obstacles))]
     obstaclesf = pygame.transform.scale(
         pygame.image.load(obstacle_.asset), (obstacle_.width, obstacle_.height)
     )
     background = pygame.image.load("assets/background.png")
     background_width = background.get_width()
 
-    gameobj = game()
     gameobj.tiles = math.ceil(game.screen_w / background_width) + 1
 
     vehicle_rect = vhsf_standing.get_rect(center=(vehicle.x, vehicle.y))
+
+    obstacle_ = obstacles[random.randrange(0, len(obstacles))]
+    obstaclesf = pygame.transform.scale(
+        pygame.image.load(obstacle_.asset), (obstacle_.width, obstacle_.height)
+    )
     obstacle_rect = obstaclesf.get_rect(center=(obstacle.x, obstacle.y))
 
     while True:
@@ -150,82 +170,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-""""
-X_POSITION, Y_POSITION = 200, 660
-
-ROCK_X_POSITION, ROCK_Y_POSITION = 600, 660
-
-O_X_POSITION = X_POSITION
-O_Y_POSITION = Y_POSITION
-
-JUMPS = False
-
-Y_GRAVITY = 0.6
-JUMP_HEIGHT = 23
-Y_VELOCITY = JUMP_HEIGHT
-
-STANDING_SURFACE = pygame.transform.scale(
-    pygame.image.load("assets/trktor_standing.png"), (99, 64)
-)
-JUMPING_SURFACE = pygame.transform.scale(
-    pygame.image.load("assets/trktor_jumping.png"), (77, 64)
-)
-ROCK_SURFACE = pygame.transform.scale(pygame.image.load("assets/rock.png"), (77, 64))
-
-BACKGROUND = pygame.image.load("assets/background.png")
-BACKGROUND_W = BACKGROUND.get_width()
-
-SCROLL = 0
-
-tiles = math.ceil(SCREEN_W / BACKGROUND_W) + 1
-
-trktor_rect = STANDING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
-rock_rect = ROCK_SURFACE.get_rect(center=(ROCK_X_POSITION, ROCK_Y_POSITION))
-
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    keys_pressed = pygame.key.get_pressed()
-
-    if trktor_rect.colliderect(rock_rect):
-        print("YOU MESSED UP")
-        break
-
-    if keys_pressed[pygame.K_SPACE]:
-        JUMPS = True
-
-    for i in range(0, tiles):
-        SCREEN.blit(BACKGROUND, (i * BACKGROUND_W + SCROLL, 0))
-
-    rock_rect.x -= 3
-    if rock_rect.x < 0:
-        rock_rect.x = ROCK_X_POSITION
-
-    SCREEN.blit(ROCK_SURFACE, rock_rect)
-
-    SCROLL -= 5
-
-    if abs(SCROLL) > BACKGROUND_W:
-        SCROLL = 0
-
-    if JUMPS:
-        Y_POSITION -= Y_VELOCITY
-        Y_VELOCITY -= Y_GRAVITY
-        if Y_VELOCITY < -JUMP_HEIGHT:
-            JUMPS = False
-            Y_VELOCITY = JUMP_HEIGHT
-
-        trktor_rect = JUMPING_SURFACE.get_rect(center=(X_POSITION, Y_POSITION))
-        SCREEN.blit(JUMPING_SURFACE, trktor_rect)
-    else:
-        trktor_rect = STANDING_SURFACE.get_rect(center=(O_X_POSITION, O_Y_POSITION))
-        SCREEN.blit(STANDING_SURFACE, trktor_rect)
-
-    pygame.display.update()
-    CLOCK.tick(60)
-"""
