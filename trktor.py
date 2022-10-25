1b6615c9a810682ffd9f182eced2617b6db28971
Michael Ablassmeier <abi@grinser.de>
2022-10-25 17:16:01 +0200

    ..

--- trktor.py
+++ trktor.py
@@ -81,11 +81,6 @@ def button(screen, position, text):
     text_render = font.render(text, 1, (255, 255, 255))
     x, y, w, h = text_render.get_rect()
     x, y = position
-    pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
-    pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
-    pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
-    pygame.draw.rect(screen, (100, 100, 100), (x, y, w, h))
     return screen.blit(text_render, (x, y))
 
 
