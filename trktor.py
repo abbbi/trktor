a5adcbf507b753439de06f6f912586002ba4cf93
ma <ma@sep.de>
2022-10-25 11:50:18 +0200

    start menu

--- trktor.py
+++ trktor.py
@@ -75,15 +75,43 @@ def load_obstacles(path):
     return obstacles
 
 
+def button(screen, position, text):
+    font = pygame.font.SysFont("Arial", 50)
+    text_render = font.render(text, 1, (255, 0, 0))
+    x, y, w, h = text_render.get_rect()
+    x, y = position
+    pygame.draw.line(screen, (150, 150, 150), (x, y), (x + w, y), 5)
+    pygame.draw.line(screen, (150, 150, 150), (x, y - 2), (x, y + h), 5)
+    pygame.draw.line(screen, (50, 50, 50), (x, y + h), (x + w, y + h), 5)
+    pygame.draw.line(screen, (50, 50, 50), (x + w, y + h), [x + w, y], 5)
+    pygame.draw.rect(screen, (100, 100, 100), (x, y, w, h))
+    return screen.blit(text_render, (x, y))
+
+
+def menu(screen):
+    b2 = button(screen, (400, 400), "Start")
+    cont = False
+    while cont is False:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if b2.collidepoint(pygame.mouse.get_pos()):
+                    cont = True
+                    break
+        pygame.display.update()
+
+
 def main():
     pygame.init()
-
     mixer.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((game.screen_w, game.screen_h))
     pygame.display.set_caption(game.caption)
 
     gameobj = game()
+    background = pygame.image.load("assets/background.png")
+    screen.blit(background, (0, 0))
+    menu(screen)
+
     mixer.music.load(f"{gameobj.media}/trktor.mp3")
     mixer.music.play()
 
@@ -112,7 +140,7 @@ def main():
     obstaclesf = pygame.transform.scale(
         pygame.image.load(obstacle_.asset), (obstacle_.width, obstacle_.height)
     )
-    background = pygame.image.load("assets/background.png")
+
     background_width = background.get_width()
 
     gameobj.tiles = math.ceil(game.screen_w / background_width) + 1
@@ -178,6 +206,7 @@ def main():
 
         pygame.display.update()
         clock.tick(gameobj.fps)
+
 
 if __name__ == "__main__":
     main()
