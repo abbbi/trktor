e33c7f7b4890e87209f17b6de6760e2e31e72244
Michael Ablassmeier <abi@grinser.de>
2022-10-26 23:22:27 +0200

    action

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,5 +1,6 @@
-irm get.scoop.sh -outfile 'install.ps1'
-.\install.ps1 -RunAsAdmin
+irm get.scoop.sh | iex
+#irm get.scoop.sh -outfile 'install.ps1'
+#.\install.ps1 -RunAsAdmin
 scoop install python@3.8.1
 git clone http://github.com/abbbi/trktor --depth 1
 cd .\trktor
