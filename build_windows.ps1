7df05ca477aaa2a36f39bae410e2af7447c4d3b5
Michael Ablassmeier <abi@grinser.de>
2022-10-26 22:58:42 +0200

    build on windows

---
+++ build_windows.ps1
@@ -0,0 +1,7 @@
+irm get.scoop.sh -outfile 'install.ps1'
+.\install.ps1 -RunAsAdmin
+scoop install python@3.8.1
+git clone http://github.com/abbbi/trktor --depth 1
+cd .\trktor
+pip install requests pygame pygame_menu pyinstaller
+pyinstaller --onefile --add-data "assets;assets" __main__.py -n trktor
