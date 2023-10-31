a39edff3f5c0e749b6caaedb21cdd175ebffd1f9
ma <ma@sep.de>
2023-10-31 10:03:49 +0100

    use venv to build

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+python3 -m venv venv
+source venv/bin/activate
 pip install pygame pygame_menu pyinstaller
 pyinstaller --onefile --add-data "assets:assets" __main__.py -n trktor
+rm -rf venv
